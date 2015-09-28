234a617676a924778a07457cbdba3263ad352b44
AJ Almacin <aldrinjerome19@gmail.com>
2015-09-28 13:12:19 -0400

    Separated view logic from controllers

--- public/js/surveys/survey.client.controller.js
+++ public/js/surveys/survey.client.controller.js
@@ -2,80 +2,84 @@
         var mainApp = angular.module("mainApp");
 
         var SurveyCtrl = function ($scope, $http, $window) {
-                var currentDate = new Date();
-                var date = currentDate.toLocaleDateString();
-                var time = currentDate.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
-                currentDate = new Date(
-                        date + "," + time
-                );
-                $scope.survey = {
-                        "activation": currentDate,
-                        "expiration": currentDate
-                };
-                $scope.questions = [];
-                $scope.errors = new Array();
+                if ($("#surveyCreatePage").length > 0) {
+                        var currentDate = new Date();
+                        var date = currentDate.toLocaleDateString();
+                        var time = currentDate.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
+                        currentDate = new Date(
+                                date + "," + time
+                        );
+                        $scope.survey = {
+                                "activation": currentDate,
+                                "expiration": currentDate
+                        };
+                        $scope.questions = [];
+                        $scope.errors = new Array();
 
-                $scope.createSurvey = function() {
-                        $http.post('/surveys', {
-                                survey: $scope.survey,
-                                questions: $scope.questions
-                        })
-                        .then(function(response) {
-                                $scope.errors = new Array();
-                                $scope.successMsg = null
-                                if(response.data.error) {
-                                        $scope.errors = response.data.errors;
-                                } else {
-                                        $scope.successMsg = "Successfully created survey.";
-                                        $scope.survey = {
-                                                "activation": currentDate,
-                                                "expiration": currentDate
-                                        };
-                                        $scope.questions = [];
+                        $scope.createSurvey = function() {
+                                $http.post('/surveys', {
+                                        survey: $scope.survey,
+                                        questions: $scope.questions
+                                })
+                                .then(function(response) {
+                                        $scope.errors = new Array();
+                                        $scope.successMsg = null
+                                        if(response.data.error) {
+                                                $scope.errors = response.data.errors;
+                                        } else {
+                                                $scope.successMsg = "Successfully created survey.";
+                                                $scope.survey = {
+                                                        "activation": currentDate,
+                                                        "expiration": currentDate
+                                                };
+                                                $scope.questions = [];
+                                        }
+
+                                }, function(response){
+                                        console.log('Error');
+                                        console.log(response);
+                                });
+                        }
+
+
+                        $scope.addAnswers = function(id) {
+                                var c=0, question = null;
+                                while( question === null && c < $scope.questions.length) {
+                                        if($scope.questions[c].id == id) {
+                                                question = $scope.questions[c];
+                                        }
+                                        c++;
                                 }
+                                question.answers.push({id: question.answers.length});
+                                return false;
+                        }
 
-                        }, function(response){
-                                console.log('Error');
-                                console.log(response);
+                        $scope.removeAnswers = function(id) {
+                                var c=0, question = null;
+                                while( question === null && c < $scope.questions.length) {
+                                        if($scope.questions[c].id == id) {
+                                                question = $scope.questions[c];
+                                        }
+                                        c++;
+                                }
+                                question.answers.pop();
+                                return false;
+                        }
+
+                        $scope.addQuestion = function() {
+                                $scope.questions.push({id: $scope.questions.length, answers: []});
+                        }
+                        $scope.removeQuestion = function() {
+                                $scope.questions.pop();
+                        }
+                }
+
+                if ($("#surveysPage").length > 0) {
+                        $http.get('/surveys').then(function(response) {
+                                $scope.surveys = response.data;
+                                console.log($scope.surveys);
                         });
                 }
-
-
-                $scope.addAnswers = function(id) {
-                        var c=0, question = null;
-                        while( question === null && c < $scope.questions.length) {
-                                if($scope.questions[c].id == id) {
-                                        question = $scope.questions[c];
-                                }
-                                c++;
-                        }
-                        question.answers.push({id: question.answers.length});
-                        return false;
-                }
-
-                $scope.removeAnswers = function(id) {
-                        var c=0, question = null;
-                        while( question === null && c < $scope.questions.length) {
-                                if($scope.questions[c].id == id) {
-                                        question = $scope.questions[c];
-                                }
-                                c++;
-                        }
-                        question.answers.pop();
-                        return false;
-                }
-
-                $scope.addQuestion = function() {
-                        $scope.questions.push({id: $scope.questions.length, answers: []});
-                }
-                $scope.removeQuestion = function() {
-                        $scope.questions.pop();
-                }
-
-                $http.get('/surveys').then(function(response) {
-                        $scope.surveys = response.data;
-                        console.log($scope.surveys);
-                });
         }
 
         mainApp.controller("SurveyCtrl", ["$scope", "$http", "$window", SurveyCtrl]);
