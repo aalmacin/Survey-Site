13cc64915f3a717069c87424fd9525bd6492c8b7
AJ Almacin <aldrinjerome19@gmail.com>
2015-09-27 22:19:32 -0400

    Added survey create file and controller

--- public/js/surveys/survey.client.controller.js
+++ public/js/surveys/survey.client.controller.js
@@ -2,7 +2,42 @@
         var mainApp = angular.module("mainApp");
 
         var SurveyCtrl = function ($scope, $http, $window) {
-                console.log("Here my firend at SurveyCtrl");
+                $scope.questions = [];
+
+                var createSurvey = function() {
+                }
+
+
+                $scope.addAnswers = function(id) {
+                        var c=0, question = null;
+                        while( question === null && c < $scope.questions.length) {
+                                if($scope.questions[c].id == id) {
+                                        question = $scope.questions[c];
+                                }
+                                c++;
+                        }
+                        question.answers.push({id: question.answers.length});
+                        return false;
+                }
+
+                $scope.removeAnswers = function(id) {
+                        var c=0, question = null;
+                        while( question === null && c < $scope.questions.length) {
+                                if($scope.questions[c].id == id) {
+                                        question = $scope.questions[c];
+                                }
+                                c++;
+                        }
+                        question.answers.push({id: question.answers.length});
+                        return false;
+                }
+
+                $scope.addQuestion = function() {
+                        $scope.questions.push({id: $scope.questions.length, answers: []});
+                }
+                $scope.removeQuestion = function() {
+                        $scope.questions.pop();
+                }
         }
 
         mainApp.controller("SurveyCtrl", ["$scope", "$http", "$window", SurveyCtrl]);
