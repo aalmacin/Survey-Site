c35909aa264a31bb84812c96f830d2de5f9cb0a4
AJ Almacin <aldrinjerome19@gmail.com>
2015-10-01 03:44:38 -0400

    Updated host and port check

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 // The file run by the nodemon/node commands
 var Server = require('./app/main.server.application')();
-Server.start(3000, 'localhost');
+var port = process.env.PORT || 3000;
+var host = process.env.HOST || 'localhost';
+Server.start(port, host);
